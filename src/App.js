46a36a8aab1b56e97a0cfd653a8de3e27d234cfc
MehdiDissem <mehdissem@gmail.com>
2022-12-04 01:26:44 +0100

    scratch

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import React from "react";
+import Login from "./components/Login/Login.jsx";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [passw, setPassw] = useState("");
+  const [dataInput, setDataInput] = useState("");
+
+  const submitThis = (e) => {
+    e.preventDefault();
+    const info = { email: email, passw: passw };
+    setDataInput([info]);
+  };
+
+  return (
+    <section className="vh-100">
+      <div className="container py-5 h-100">
+        <div className="row d-flex align-items-center justify-content-center h-100">
+          <div className="col-md-8 col-lg-7 col-xl-6">
+            <img
+              src="https://images.squarespace-cdn.com/content/v1/58a1fe895016e192bf28ff7f/1568646848366-IYI7DZMOFU5L7AMJ2CTC/Eco.png?format=1500w"
+              className="img-fluid"
+              alt="x"
+            />
+          </div>
+          <div className="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+            <form onSubmit={(e) => submitThis(e)}>
+              {/* <!-- Email input --> */}
+              <div className="form-outline mb-4">
+                <input
+                  type="email"
+                  id="form1Example13"
+                  className="form-control form-control-lg"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <label className="form-label">Email address</label>
+              </div>
+
+              {/* <!-- Password input --> */}
+              <div className="form-outline mb-4">
+                <input
+                  type="password"
+                  className="form-control form-control-lg"
+                  onChange={(e) => setPassw(e.target.value)}
+                />
+                <label className="form-label">Password</label>
+              </div>
+
+              <div className="d-flex justify-content-around align-items-center mb-4">
+                {/* <!-- Checkbox --> */}
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="form1Example3"
+                  />
+                  <label className="form-check-label"> Remember me </label>
+                </div>
+                <a href="#!">Forgot password?</a>
+              </div>
+
+              {/* <!-- Submit button --> */}
+              <button
+                type="submit"
+                className="btn btn-primary btn-lg btn-block"
+                style={{ backgroundColor: "#77DD77", borderColor: "#77DD77" }}
+              >
+                Sign in
+              </button>
+
+              <div className="divider d-flex align-items-center my-4">
+                <p className="text-center fw-bold mx-3 mb-0 text-muted">OR</p>
+              </div>
+
+              <a
+                className="btn btn-primary btn-lg btn-block"
+                style={{ backgroundColor: "#77DD77", borderColor: "#77DD77" }}
+                href="#!"
+                role="button"
+              >
+                <i className="fab fa-facebook-f me-2"></i>Continue with Facebook
+              </a>
+              <a
+                className="btn btn-primary btn-lg btn-block"
+                style={{ backgroundColor: "#77DD77", borderColor: "#77DD77" }}
+                href="#!"
+                role="button"
+              >
+                <i className="fab fa-twitter me-2"></i>Continue with Twitter
+              </a>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
