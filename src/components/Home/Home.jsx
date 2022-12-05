d13601b7fdb4e53aae921682c3dab5704809600c
MehdiDissem <mehdissem@gmail.com>
2022-12-05 11:46:45 +0100

    need to fix header

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,19 +4,6 @@ import { getAuth, signOut } from "firebase/auth";
 import OurNavbar from "../NavBar/Navbar.jsx";
 
 const Home = () => {
-  const [view, setView] = useState("Home");
-  const disconnect = () => {
-    // const auth = getAuth();
-    const auth = getAuth();
-    signOut(auth)
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
   return (
     <div>
       {/* <!--Navbar --> */}
